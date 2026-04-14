a19490fd85d7d852dd5f716358837631ced23b05
lufiya04 <your-email@example.com>
2026-04-14 23:44:31 +0530

    fix:GitHub Pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(new URL(".", import.meta.url)));
 export default defineConfig({
   base: "/Lufiya-Portfolio/",
   server: {
-    host: "0.0.0.0",
+    host: "::",
     port: 8080,
   },
   plugins: [react()],
